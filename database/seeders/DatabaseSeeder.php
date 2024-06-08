d2db78123044be8ec7775e50961751e456b744b8
saktiworkstation <saktiworkstation@gmail.com>
2024-06-08 16:52:21 +0700

    added user seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +19,13 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::create([
+            'name' => 'Sakti',
+            'username' => 'sakti',
+            'email' => 'skarluajitkas@gmail.com',
+            'password' => bcrypt('password'),
+            'role' => 'super-admin'
+        ]);
     }
 }
