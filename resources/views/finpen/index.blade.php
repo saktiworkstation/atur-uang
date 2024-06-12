eae464875d762ee909d5eade324ef26e078ca002
saktiworkstation <saktiworkstation@gmail.com>
2024-06-12 17:16:04 +0700

    added table data template

--- resources/views/finpen/index.blade.php
+++ resources/views/finpen/index.blade.php
@@ -18,17 +18,57 @@
                     <h4 class="pb-3 font-semibold text-xl text-gray-800 leading-tight">
                         {{ __('Finpen Data') }}
                     </h4>
-                    <table class="border-collapse border-slate-500">
-                        <thead class="bg-gray-800 text-white">
-                            <tr>
-                                <th class="px-6 py-2 border border-slate-600">Interest</th>
-                                <th class="px-6 py-2 border border-slate-600">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody class="bg-gray-500">
-                            data disini
-                        </tbody>
-                    </table>
+
+
+
+                    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead
+                                class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        Product name
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Color
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Category
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Price
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        <span class="sr-only">Edit</span>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr
+                                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                    <th scope="row"
+                                        class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        Apple MacBook Pro 17"
+                                    </th>
+                                    <td class="px-6 py-4">
+                                        Silver
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        Laptop
+                                    </td>
+                                    <td class="px-6 py-4">
+                                        $2999
+                                    </td>
+                                    <td class="px-6 py-4 text-right">
+                                        <a href="#"
+                                            class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+
                 </div>
             </div>
         </div>
