abb28a0b678b10c6455e4b63c70f97e62258bb65
saktiworkstation <saktiworkstation@gmail.com>
2024-06-25 23:04:31 +0700

    Create Like Controller

---
+++ app/Http/Controllers/LikeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LikeController extends Controller
+{
+    public function store(Request $request, $id){
+        //
+    }
+
+    public function destroy($id){
+        //
+    }
+}
