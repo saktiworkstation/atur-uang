2914ac56ae46a0e6fa53ee1bb46f487887a4bc9b
saktiworkstation <saktiworkstation@gmail.com>
2024-06-25 22:55:51 +0700

    fill the migration

--- database/migrations/2024_06_25_154135_create_likes_table.php
+++ database/migrations/2024_06_25_154135_create_likes_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('likes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('finpen_id');
             $table->timestamps();
         });
     }
