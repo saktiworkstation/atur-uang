7c997ad67b239e46fd6cf323cff5c58004ea3244
saktiworkstation <saktiworkstation@gmail.com>
2024-06-26 14:32:36 +0700

    finpen : menambahkan tombol hapus

--- resources/views/finpen/index.blade.php
+++ resources/views/finpen/index.blade.php
@@ -46,6 +46,13 @@
                                         class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                         Edit
                                     </a>
+                                    <form action="/finpen/{{ $item->id }}/delete" method="post" class="d-inline">
+                                        @method('delete')
+                                        @csrf
+                                        <x-delete-button :message="$item->finpen_name">
+                                            Delete
+                                        </x-delete-button>
+                                    </form>
                                 </div>
                             @endforeach
                         @else
