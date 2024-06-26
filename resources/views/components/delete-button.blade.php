0eccf26e702aff005ef15274dab14b4d049fe35e
saktiworkstation <saktiworkstation@gmail.com>
2024-06-26 14:32:15 +0700

    component : mengubah warna

--- resources/views/components/delete-button.blade.php
+++ resources/views/components/delete-button.blade.php
@@ -1,7 +1,7 @@
 @props(['message'])
 
 <button
-    {{ $attributes->merge(['class' => 'inline-flex items-center px-3 py-1 bg-gray-800 border border-transparent rounded-md font-medium text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}
+    {{ $attributes->merge(['class' => 'mt-1 inline-flex items-center px-3 py-1 bg-rose-600 border border-transparent rounded-md font-medium text-xs text-white uppercase tracking-widest hover:bg-rose-500 focus:bg-rose-500 active:bg-rose-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}
     onclick="return confirm('Are you sure want to delete {{ $message }}?')">
     {{ $slot }}
 </button>
